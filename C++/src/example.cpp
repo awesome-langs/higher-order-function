f5a3816dbf463f32944a5e92391c0599d7d22ea4
slzxcdw <798759765@.com>
2024-03-21 00:12:29 +0800

    update: C++ reference update

--- C++/src/example.cpp
+++ C++/src/example.cpp
@@ -27,29 +27,29 @@ string p_e_escapeString(const string& s) {
     return ranges::fold_left(res | views::join_with(""sv), string(), plus());
 }
 
-function<string(bool)> p_e_bool() {
-    return [](auto b) { return b ? "true" : "false"; };
+function<string(const bool&)> p_e_bool() {
+    return [](const bool& b) { return b ? "true" : "false"; };
 }
 
-function<string(int)> p_e_int() {
-    return [](auto i) { return to_string(i); };
+function<string(const int&)> p_e_int() {
+    return [](const int& i) { return to_string(i); };
 }
 
-function<string(double)> p_e_double() {
-    return [](auto d) { 
+function<string(const double&)> p_e_double() {
+    return [](const double& d) { 
         string s0 = format("{:.7f}", d);
         string s1 = s0.substr(0, s0.length() - 1);
         return (s1 == "-0.000000") ? "0.000000" : s1;
     };
 }
 
-function<string(string)> p_e_string() {
-    return [](auto s) { return "\"" + p_e_escapeString(s) + "\""; };
+function<string(const string&)> p_e_string() {
+    return [](const string& s) { return "\"" + p_e_escapeString(s) + "\""; };
 }
 
 template <typename V>
-function<string(vector<V>)> p_e_list(function<string(V)> f0) {
-    return [f0 = move(f0)](vector<V> lst) -> string {
+function<string(const vector<V>&)> p_e_list(function<string(const V&)> f0) {
+    return [f0 = move(f0)](const vector<V>& lst) -> string {
         // vector<string> vs = lst | views::transform(f0) | ranges::to<vector<string>>();
         auto tmp = lst | views::transform(f0);
         vector<string> vs = vector<string>(tmp.begin(), tmp.end());
@@ -58,8 +58,8 @@ function<string(vector<V>)> p_e_list(function<string(V)> f0) {
 }
 
 template <typename V>
-function<string(vector<V>)> p_e_ulist(function<string(V)> f0) {
-    return [f0 = move(f0)](vector<V> lst) -> string {
+function<string(const vector<V>&)> p_e_ulist(function<string(const V&)> f0) {
+    return [f0 = move(f0)](const vector<V>& lst) -> string {
         // vector<string> vs = lst | views::transform(f0) | ranges::to<vector<string>>();
         auto tmp = lst | views::transform(f0);
         vector<string> vs = vector<string>(tmp.begin(), tmp.end());
@@ -69,9 +69,9 @@ function<string(vector<V>)> p_e_ulist(function<string(V)> f0) {
 }
 
 template <typename V>
-function<string(unordered_map<int, V>)> p_e_idict(function<string(V)> f0) {
+function<string(const unordered_map<int, V>&)> p_e_idict(function<string(const V&)> f0) {
     function<string(pair<int, V>)> f1 = [f0 = move(f0)](auto kv){return p_e_int()(kv.first) + "=>" + f0(kv.second); };
-    return [f1 = move(f1)](unordered_map<int, V> dct) -> string {
+    return [f1 = move(f1)](const unordered_map<int, V>& dct) -> string {
         // vector<string> vs = dct | views::transform(f1) | ranges::to<vector<string>>();
         auto tmp = dct | views::transform(f1);
         vector<string> vs = vector<string>(tmp.begin(), tmp.end());
@@ -81,9 +81,9 @@ function<string(unordered_map<int, V>)> p_e_idict(function<string(V)> f0) {
 }
 
 template <typename V>
-function<string(unordered_map<string, V>)> p_e_sdict(function<string(V)> f0) {
+function<string(const unordered_map<string, V>&)> p_e_sdict(function<string(const V&)> f0) {
     function<string(pair<string, V>)> f1 = [f0 = move(f0)](auto kv){return p_e_string()(kv.first) + "=>" + f0(kv.second); };
-    return [f1 = move(f1)](unordered_map<string, V> dct) -> string {
+    return [f1 = move(f1)](const unordered_map<string, V>& dct) -> string {
         // vector<string> vs = dct | views::transform(f1) | ranges::to<vector<string>>();
         auto tmp = dct | views::transform(f1);
         vector<string> vs = vector<string>(tmp.begin(), tmp.end());
@@ -93,8 +93,8 @@ function<string(unordered_map<string, V>)> p_e_sdict(function<string(V)> f0) {
 }
 
 template <typename V>
-function<string(optional<V>)> p_e_option(function<string(V)> f0) {
-    return [f0 = move(f0)](optional<V> opt) -> string {
+function<string(const optional<V>&)> p_e_option(function<string(const V&)> f0) {
+    return [f0 = move(f0)](const optional<V>& opt) -> string {
         if (opt.has_value()) {
             return f0(opt.value());
         } else {
--- Rust/src/example.rs
+++ Rust/src/example.rs
@@ -11,31 +11,31 @@ fn p_e_escape_string(s: &String) -> String {
     s.chars().map(p_e_escape_char).collect::<Vec<String>>().join("")
 }
 
-fn p_e_bool() -> impl Fn(bool) -> String {
-    |b| if b { "true".to_string() } else { "false".to_string() }
+fn p_e_bool() -> impl Fn(&bool) -> String {
+    |b| if *b { "true".to_string() } else { "false".to_string() }
 }
 
-fn p_e_int() -> impl Fn(i32) -> String {
-    |i| i.to_string()
+fn p_e_int() -> impl Fn(&i32) -> String {
+    |i| (*i).to_string()
 }
 
-fn p_e_double() -> impl Fn(f64) -> String {
+fn p_e_double() -> impl Fn(&f64) -> String {
     |d| {
-        let s0 = format!("{:.7}", d);
+        let s0 = format!("{:.7}", *d);
         let s1 = s0[..s0.len()-1].to_string();
         if s1 == "-0.000000" { "0.000000".to_string() } else { s1 }
     }
 }
 
-fn p_e_string() -> impl Fn(String) -> String {
-    |s| format!("\"{}\"", p_e_escape_string(&s))
+fn p_e_string() -> impl Fn(&String) -> String {
+    |s| format!("\"{}\"", p_e_escape_string(s))
 }
 
-fn p_e_list<V>(f0: impl Fn(V) -> String) -> impl Fn(Vec<V>) -> String {
+fn p_e_list<V>(f0: impl Fn(&V) -> String) -> impl Fn(&Vec<V>) -> String {
     move |lst| format!("[{}]", lst.into_iter().map(&f0).collect::<Vec<String>>().join(", "))
 }
 
-fn p_e_ulist<V>(f0: impl Fn(V) -> String) -> impl Fn(Vec<V>) -> String {
+fn p_e_ulist<V>(f0: impl Fn(&V) -> String) -> impl Fn(&Vec<V>) -> String {
     move |lst| { 
         let mut vs = lst.into_iter().map(&f0).collect::<Vec<String>>(); 
         vs.sort(); 
@@ -43,25 +43,25 @@ fn p_e_ulist<V>(f0: impl Fn(V) -> String) -> impl Fn(Vec<V>) -> String {
     }
 }
 
-fn p_e_idict<V>(f0: impl Fn(V) -> String) -> impl Fn(HashMap<i32, V>) -> String {
+fn p_e_idict<V>(f0: impl Fn(&V) -> String) -> impl Fn(&HashMap<i32, V>) -> String {
     let f1 = move |(k, v)| format!("{}=>{}", p_e_int()(k), f0(v));
-    move |dct| {        
+    move |dct| {
         let mut vs = dct.into_iter().map(&f1).collect::<Vec<String>>();
         vs.sort();
         format!("{{{}}}", vs.join(", "))
     }
 }
 
-fn p_e_sdict<V>(f0: impl Fn(V) -> String) -> impl Fn(HashMap<String, V>) -> String {
-    let f1 = move |(k, v)| format!("{}=>{}", p_e_string()(k), f0(v));
-    move |dct| {        
+fn p_e_sdict<V>(f0: impl Fn(&V) -> String) -> impl Fn(&HashMap<String, V>) -> String {
+    move |dct| {
+        let f1 = |(k, v)| format!("{}=>{}", p_e_string()(k), f0(v));
         let mut vs = dct.into_iter().map(&f1).collect::<Vec<String>>();
         vs.sort();
         format!("{{{}}}", vs.join(", "))
     }
 }
 
-fn p_e_option<V>(f0: impl Fn(V) -> String) -> impl Fn(Option<V>) -> String {
+fn p_e_option<V>(f0: impl Fn(&V) -> String) -> impl Fn(&Option<V>) -> String {
     move |opt| match opt {
         Some(x) => f0(x),
         None => "null".to_string()
@@ -70,39 +70,39 @@ fn p_e_option<V>(f0: impl Fn(V) -> String) -> impl Fn(Option<V>) -> String {
 
 fn main() {
     let p_e_out = vec![
-            p_e_bool()(true),
-            p_e_bool()(false),
-            p_e_int()(3),
-            p_e_int()(-107),
-            p_e_double()(0.0),
-            p_e_double()(-0.0),
-            p_e_double()(3.0),
-            p_e_double()(31.4159265),
-            p_e_double()(123456.789),
-            p_e_string()("Hello, World!".to_string()),
-            p_e_string()("!@#$%^&*()[]{}<>:;,.'\"?|".to_string()),
-            p_e_string()("/\\\n\t".to_string()),
-            p_e_list(p_e_int())(vec![]),
-            p_e_list(p_e_int())(vec![1, 2, 3]),
-            p_e_list(p_e_bool())(vec![true, false, true]),
-            p_e_list(p_e_string())(vec!["apple".to_string(), "banana".to_string(), "cherry".to_string()]),
-            p_e_list(p_e_list(p_e_int()))(vec![]),
-            p_e_list(p_e_list(p_e_int()))(vec![vec![1, 2, 3], vec![4, 5, 6]]),
-            p_e_ulist(p_e_int())(vec![3, 2, 1]),
-            p_e_list(p_e_ulist(p_e_int()))(vec![vec![2, 1, 3], vec![6, 5, 4]]),
-            p_e_ulist(p_e_list(p_e_int()))(vec![vec![4, 5, 6], vec![1, 2, 3]]),
-            p_e_idict(p_e_int())(HashMap::from([])),
-            p_e_idict(p_e_string())(HashMap::from([(1, "one".to_string()), (2, "two".to_string())])),
-            p_e_sdict(p_e_int())(HashMap::from([("one".to_string(), 1), ("two".to_string(), 2)])),
-            p_e_idict(p_e_list(p_e_int()))(HashMap::from([])),
-            p_e_idict(p_e_list(p_e_int()))(HashMap::from([(1, vec![1, 2, 3]), (2, vec![4, 5, 6])])),
-            p_e_sdict(p_e_list(p_e_int()))(HashMap::from([("one".to_string(), vec![1, 2, 3]), ("two".to_string(), vec![4, 5, 6])])),
-            p_e_list(p_e_idict(p_e_int()))(vec![HashMap::from([(1, 2)]), HashMap::from([(3, 4)])]),
-            p_e_idict(p_e_idict(p_e_int()))(HashMap::from([(1, HashMap::from([(2, 3)])), (4, HashMap::from([(5, 6)]))])),
-            p_e_sdict(p_e_sdict(p_e_int()))(HashMap::from([("one".to_string(), HashMap::from([("two".to_string(), 3)])), ("four".to_string(), HashMap::from([("five".to_string(), 6)]))])),
-            p_e_option(p_e_int())(Some(42)),
-            p_e_option(p_e_int())(None),
-            p_e_list(p_e_option(p_e_int()))(vec![Some(1), None, Some(3)])
+            p_e_bool()(&true),
+            p_e_bool()(&false),
+            p_e_int()(&3),
+            p_e_int()(&-107),
+            p_e_double()(&0.0),
+            p_e_double()(&-0.0),
+            p_e_double()(&3.0),
+            p_e_double()(&31.4159265),
+            p_e_double()(&123456.789),
+            p_e_string()(&"Hello, World!".to_string()),
+            p_e_string()(&"!@#$%^&*()[]{}<>:;,.'\"?|".to_string()),
+            p_e_string()(&"/\\\n\t".to_string()),
+            p_e_list(p_e_int())(&vec![]),
+            p_e_list(p_e_int())(&vec![1, 2, 3]),
+            p_e_list(p_e_bool())(&vec![true, false, true]),
+            p_e_list(p_e_string())(&vec!["apple".to_string(), "banana".to_string(), "cherry".to_string()]),
+            p_e_list(p_e_list(p_e_int()))(&vec![]),
+            p_e_list(p_e_list(p_e_int()))(&vec![vec![1, 2, 3], vec![4, 5, 6]]),
+            p_e_ulist(p_e_int())(&vec![3, 2, 1]),
+            p_e_list(p_e_ulist(p_e_int()))(&vec![vec![2, 1, 3], vec![6, 5, 4]]),
+            p_e_ulist(p_e_list(p_e_int()))(&vec![vec![4, 5, 6], vec![1, 2, 3]]),
+            p_e_idict(p_e_int())(&HashMap::from([])),
+            p_e_idict(p_e_string())(&HashMap::from([(1, "one".to_string()), (2, "two".to_string())])),
+            p_e_sdict(p_e_int())(&HashMap::from([("one".to_string(), 1), ("two".to_string(), 2)])),
+            p_e_idict(p_e_list(p_e_int()))(&HashMap::from([])),
+            p_e_idict(p_e_list(p_e_int()))(&HashMap::from([(1, vec![1, 2, 3]), (2, vec![4, 5, 6])])),
+            p_e_sdict(p_e_list(p_e_int()))(&HashMap::from([("one".to_string(), vec![1, 2, 3]), ("two".to_string(), vec![4, 5, 6])])),
+            p_e_list(p_e_idict(p_e_int()))(&vec![HashMap::from([(1, 2)]), HashMap::from([(3, 4)])]),
+            p_e_idict(p_e_idict(p_e_int()))(&HashMap::from([(1, HashMap::from([(2, 3)])), (4, HashMap::from([(5, 6)]))])),
+            p_e_sdict(p_e_sdict(p_e_int()))(&HashMap::from([("one".to_string(), HashMap::from([("two".to_string(), 3)])), ("four".to_string(), HashMap::from([("five".to_string(), 6)]))])),
+            p_e_option(p_e_int())(&Some(42)),
+            p_e_option(p_e_int())(&None),
+            p_e_list(p_e_option(p_e_int()))(&vec![Some(1), None, Some(3)])
         ].join("\n");
     std::fs::write("stringify.out", p_e_out).unwrap();
 }
